3d1afbd91d78fb13084d5da4ae2207e4f727beef
Varunkumar23 <sarabuvarunkumar@gmail.com>
2025-11-09 22:26:40 +0530

    env is created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative AI Project",
+    version='0.0.0',
+    author='Varun',
+    author_email='Varun@neuron.ai',
+    packages=find_packages(),
+    install_requires=[]
+)
